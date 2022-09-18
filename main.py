916906be9f770c482005e81717f476a9c2c03915
BJL156 <bjl15634@gmail.com>
2022-09-18 14:41:56 -0400

    Fixed Colors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', '@', ',', '.']
 # ASCII_CHARS = [".", ",", ":", ";", "+", "*", "?", "%", "S", "#", "@"]
 
-scale_factor = 0.2
+scale_factor = 0.25
 
 text = open("output.txt", 'w')
 
@@ -17,6 +17,7 @@ def gen_frame(image):
     width, height = image.size
     image = image.resize((int(scale_factor*width), int(scale_factor*height)), Image.NEAREST)
     width, height = image.size
+
     pixels = image.load()
 
     for y in range(height):
@@ -27,17 +28,22 @@ def gen_frame(image):
 
             percent = (gray / 255 * 100)
             # print(ASCII_CHARS[int(percent / len(ASCII_CHARS)) - 1], end='')
+
             print(colored(r, g, b, ASCII_CHARS[int(percent / len(ASCII_CHARS)) - 1]), end='')
             text.write(f"{ASCII_CHARS[int(percent / len(ASCII_CHARS)) - 1]}")
         print('')
         text.write('\n')
+    
+    image.save("output.jpg")
 
-cap = cv2.VideoCapture(input("video file path: "))
+filepath = input("video file path: ")
+cap = cv2.VideoCapture(filepath)
 input_1 = input("show video in cv2 window? (y/n): ")
 
 while True:
     ret, frame = cap.read()
     if input_1 == 'y': cv2.imshow('frame', frame)
-    gen_frame(Image.fromarray(frame))
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    gen_frame(Image.fromarray(frame_rgb))
     cv2.waitKey(1)
     os.system('cls')
